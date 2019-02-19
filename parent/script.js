861371fc71a5340b6ff87420500234418d57de2b
Dom Gaulton <dom.gaulton@hub-box.com>
2019-02-19 10:52:51 +0000

    Updating parent style

--- parent/script.js
+++ parent/script.js
@@ -8,4 +8,14 @@ const messageDisplay = document.getElementById('display');
 eventer(messageEvent,function(e) {
   messageDisplay.innerHTML = e.data;
   // console.log('parent received message!:  ',e.data);
+
+  modal.style.display ='none';
 },false);
+
+
+// UX
+const clickAndCollect = document.getElementById('cnc');
+const modal = document.getElementById('modal');
+clickAndCollect.addEventListener('click', function(){
+  modal.style.display ='block';
+})
